b0e4a6552f5e58aa2fcfecd16f9feb230f4ae55a
Hamid Tavakoli <info@ai-fairness.com>
2026-08-07 18:37:55 +0100

    Delete examples directory

--- examples/init.py
+++
@@ -1,8 +0,0 @@
-"""
-BiasClean v2.0 Examples Package
-Collection of usage examples and demonstrations
-"""
-
-__version__ = "2.0"
-__author__ = "AI-Fairness-com"
-__description__ = "BiasClean v2.0 usage examples"
